dd989d44ac6a43c65528285aab160fa5198bdb06
Arthur-Paulino <128192898+Arthur-Paulino@users.noreply.github.com>
2023-04-06 00:15:26 +0000

    Pending changes exported from your codespace

--- sketch.js
+++ sketch.js
@@ -5,10 +5,8 @@ function preload(){
   // Descomente o código para adicionar animação ao navio
 
   shipImg1 = loadAnimation("ship-1.png");
-  //shipImg1 = loadAnimation("ship-1.png");
-  //shipImg1 = loadAnimation("ship-1");
-  //shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
-  //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+ shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+ 
   
   seaImg = loadImage("sea.png");
 }
@@ -36,10 +34,8 @@ function draw() {
 
   // Descomente o código para redefinir o fundo
   if(sea.x < 0){
-    //sea.x = 0;
-    //sea.x = sea.width;
-    //sea.x = sea.width/8;
-    //sea.y = height;
+  sea.x = sea.width/8;
+    
   }
 
  
